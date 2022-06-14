ae93aafc60de8d5d56055749f5ff7b1008a34c4b
Rodrigues <ipdalimeira@gmail.com>
2022-06-14 07:46:35 -0300

    Concertando o codigo

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 let nome = "Rafael"
-console.log(nome);
 let sobrenome = "Ferreira Rodrigues"
-console.log(sobrenome);
 let anoAtual = 2022
 let anoNascimento = 1991
-console.log(anoAtual-anoNascimento);
 let cidade = "Limeira-SP"
-console.log(cidade);
 let profissao = "Estudante"
-console.log(profissao);
 let estudante = "Desenvolvedor Full Stack Kenzie Academy Brasil"
-console.log(estudante);
-
-
-
+let meusDados = `${nome} ${sobrenome} ${anoAtual-anoNascimento} anos, vive em ${cidade} - ${profissao} ${estudante}`
+console.log (meusDados)
