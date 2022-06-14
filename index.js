c7eb162a4cb49eb2d505b6da90bd8cf741ad40a2
Rafael Rodrigues <106622610+ipdalimeira@users.noreply.github.com>
2022-06-14 07:06:24 -0300

    Entrega praticando as variaveis do JS

---
+++ index.js
@@ -0,0 +1,16 @@
+let nome = "Rafael"
+console.log(nome);
+let sobrenome = "Ferreira Rodrigues"
+console.log(sobrenome);
+let anoAtual = 2022
+let anoNascimento = 1991
+console.log(anoAtual-anoNascimento);
+let cidade = "Limeira-SP"
+console.log(cidade);
+let profissao = "Estudante"
+console.log(profissao);
+let estudante = "Desenvolvedor Full Stack Kenzie Academy Brasil"
+console.log(estudante);
+
+
+
